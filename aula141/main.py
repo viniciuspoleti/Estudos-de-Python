363bb14812efc32a18f73bc8edf6b7038da402a4
Vinicius Poleti <poleti.vinicius@gmail.com>
2024-07-22 11:21:44 -0300

    Add files via upload

---
+++ aula141/main.py
@@ -0,0 +1,9 @@
+from log import LogFileMixin, LogPrintMixin
+from eletronico import Smartphone
+
+galaxy_s = Smartphone('Galaxy S')
+iphone = Smartphone('iPhone')
+
+galaxy_s.ligar()
+iphone.ligar()
+iphone.desligar()
